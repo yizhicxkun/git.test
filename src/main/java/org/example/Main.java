82bd55c41607d4ff779c0fd1cfbe1cd8e69bfb04
fzxj <3116812033@qq.com>
2023-07-08 21:45:39 +0800

    first

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!1");
            System.out.println("kun");
+        System.out.println("kun1");
     }
 }
